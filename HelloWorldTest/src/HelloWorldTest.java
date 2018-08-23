7f39cf72ddc61b4f66ece56d4567a4a4ff154fe0
Saransh Gupta <saranshg@amazon.com>
2018-08-23 19:09:48 +0530

    Testing changes on release

--- HelloWorldTest/src/HelloWorldTest.java
+++ HelloWorldTest/src/HelloWorldTest.java
@@ -3,6 +3,7 @@ public class HelloWorldTest {
     public static String messageHelloWorldTest() {
 
         String message = "Hello World..!!";
+        String message2 = "Hello again..!!";
         return message;
     }
 }
